2c51932e80801a9353a8abc114bddf6ff8584342
Elitezen <dev.alejandromarin@gmail.com>
2024-12-22 21:23:58 -0500

    Adjustments and bug fixes

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -4,7 +4,7 @@ function Footer() {
     return (
         <footer>
             <div className={styles.content}>
-                <h4>Want to join the voyage? Feel free to reach out</h4>
+                <h4>Want to join the voyage? Feel free to <span>reach out</span></h4>
             </div>
         </footer>
     )
