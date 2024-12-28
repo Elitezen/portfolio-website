0b2e82ee3bdc4a619685560a7322783f0b5dae5a
Elitezen <dev.alejandromarin@gmail.com>
2024-12-28 10:47:43 -0500

    Added Scriptly Studios cover

--- src/pages/Home/sections/MyWork/sections/HighlightedProjects/index.tsx
+++ src/pages/Home/sections/MyWork/sections/HighlightedProjects/index.tsx
@@ -2,6 +2,7 @@ import ProjectCard from "../../components/ProjectCard";
 import styles from "./index.module.scss";
 
 import KanaFlowCover from "../../../../../../assets/images/covers/kana-flow.png";
+import ScriptlyStudiosCover from "../../../../../../assets/images/covers/scriptly-studios.png";
 
 function HighlightedProjects() {
     return (
@@ -9,6 +10,20 @@ function HighlightedProjects() {
             <h1>Highlighted Projects</h1>
 
             <div className={styles.projectCards}>
+                <ProjectCard 
+                    imgSrc={ScriptlyStudiosCover}
+                    title="Scriptly Studios"
+                    subTitle="Full-Stack Developer | Freelancer"
+                    description="Developed the official website, moderation system, and a promotional code system which successfully served 15,000 codes"
+                    technologies={[
+                        "React", "SCSS", "Node.JS", "TypeScript", "Express API", "MongoDB"
+                    ]}
+                    bulletPoints={[
+                        "Designed and developed a webpage visited by hundreds.",
+                        "Produced tools used to moderate a Discord server of 18,000 users.",
+                        "Developed and deployed a promotional code API and database that served 15,000 players with a %100 uptime."
+                    ]}/>
+
                 <ProjectCard 
                     imgSrc={KanaFlowCover}
                     title="KanaFlow"
@@ -26,25 +41,11 @@ function HighlightedProjects() {
 
                 <ProjectCard 
                     imgSrc={KanaFlowCover}
-                    title="Scriptly Studios"
-                    subTitle="Full-Stack Developer | Freelancer"
-                    description="Developed the official website, moderation system, and a promotional code system which successfully served 15,000 codes"
-                    technologies={[
-                        "React", "SCSS", "Node.JS", "TypeScript", "MongoDB"
-                    ]}
-                    bulletPoints={[
-                        "Designed and developed a webpage visited by hundreds.",
-                        "Produced tools used to moderate a Discord server of 18,000 users.",
-                        "Developed and deployed a promotional code API and database that served 15,000 players with a %100 uptime."
-                    ]}/>
-
-                <ProjectCard 
-                    imgSrc={KanaFlowCover}
                     title="Expressia"
                     subTitle="Full-Stack Developer"
-                    description="A mobile app for practicing Japanese Kana memorization. Complemented by a webpage."
+                    description="An innovative Discord bot for browsing and importing emojis within Discord."
                     technologies={[
-                        "React", "SCSS", "Node.JS", "TypeScript", "Express API"
+                        "NodeJS", "TypeScript", "React", "SCSS"
                     ]}
                     bulletPoints={[
                         "Reached XXXX server invites",
